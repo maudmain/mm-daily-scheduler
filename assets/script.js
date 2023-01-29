4b21756d6d55ef8d07a1e9f71c2f4ce702acb3c2
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-29 10:14:10 +0000

    add bootstrap classes

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,14 @@ $(document).ready(function () {
 });
 
 // create timeblocks
+
+//create a form element to hold all timeblocks
+let formEl= $('<form>');
+formEl.addClass('form')
+containerEl.append(formEl)
+
+// create an array for the hours
+//assuming one timeblock represents one hour
 const timeblocks = [
     9,
     10,
@@ -23,22 +31,24 @@ const timeblocks = [
     16
 ];
 
+// forEach loop to create the elements, add the boostrap classes and append to the parent element
 timeblocks.forEach((timeblock) => {
     let timeblocksDiv = $('<div>');
-    timeblocksDiv.addClass('timeblock');
-    containerEl.append(timeblocksDiv);
+    timeblocksDiv.addClass('row border rounded ');
+    formEl.append(timeblocksDiv);
 
     let divTime = $('<span>');
+    divTime.addClass('col-2 text-center ')
     divTime.text(moment().hour(timeblock).minute(0).second(0).format("LT"));
     timeblocksDiv.append(divTime);
 
-
     let divInput = $('<input>');
-    divInput.addClass('inputText');
+    divInput.addClass('inputText col-9 ');
     timeblocksDiv.append(divInput);
 
     let divButton = $('<button>');
-    divButton.addClass('saveButton');
+    divButton.addClass('btn btn-primary col-1  text-center');
+    divButton.text('Save')
     timeblocksDiv.append(divButton);
 })
 
